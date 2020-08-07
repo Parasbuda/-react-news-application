19bf55d33acad371601b4b1f39a83fc4b0ed9293
Paras Buda <parasbuda1234@gmail.com>
2020-08-08 00:40:13 +0545

    completed

--- src/App.js
+++ src/App.js
@@ -2,26 +2,46 @@ import React, { useState, useEffect } from "react"
 import "./App.css"
 import alanBtn from "@alan-ai/alan-sdk-web"
 import NewsCards from "./component/NewsCards/NewsCards"
+import useStyles from "./styles"
+import wordsToNumbers from "words-to-numbers"
 
 const alanKey =
   "2b6bb9e355e256c7962d952f851c8a5b2e956eca572e1d8b807a3e2338fdd0dc/stage"
 
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([])
+  const [activeArticle,setActiveArticle]=useState(-1)
+  const classes=useStyles()
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles,number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles)
+          setActiveArticle(-1)
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle)=>prevActiveArticle+1)
+        } else if (command === "open") {
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number,{fuzzy:true}) : number
+          const article = articles[parsedNumber - 1]
+          
+          if (parsedNumber > 20) {
+            alanBtn().playText("please try again")
+          } else if(article){
+            window.open(article.url, '_blank')
+            alanBtn().playText("Opening...")
+          }
+          
         }
       },
     })
   }, [])
   return (
     <div>
-      <h1>Alan news app</h1>
-      <NewsCards articles={newsArticles} />
+      <div className={classes.logoContainer}>
+        <img src="https://portfolio-paras.netlify.app/images/skill.jpeg" className={classes.logo} alt="logo"/>
+     </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   )
 }
--- src/component/NewsCard/NewsCard.js
+++ src/component/NewsCard/NewsCard.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React,{useState,useEffect,createRef} from "react"
 import {
   Card,
   CardActions,
@@ -8,24 +8,37 @@ import {
   Button,
   Typography,
 } from "@material-ui/core"
-
+import classNames from "classnames"
 import useStyles from "./styles"
 
 const NewsCard = ({
   article: { description, publishedAt, source, title, url, urlToImage },
-  i,
+  i,activeArticle
 }) => {
-    const classes=useStyles()
+    const classes = useStyles()
+    const [elRefs, setElRefs] = useState([])
+    const scrollToRef = (ref) =>window.scroll(0,ref.current.offsetTop-50)
+        
+    
+    useEffect(() => {
+        setElRefs((refs)=>Array(20).fill.map((_,j)=>refs[j]|| createRef()))
+    }, [])
+    
+    useEffect(() => {
+        if (i === activeArticle && elRefs[activeArticle]) {
+            scrollToRef(elRefs[activeArticle])
+        }
+    },[i,activeArticle,elRefs])
   return (
-    <Card>
-      <CardActionArea>
+    <Card ref={elRefs[i]} className={classNames(classes.card,activeArticle===i?classes.activeCard:null)}>
+      <CardActionArea href={url} target="_blank">
         <CardMedia className={classes.media}
           image={
             urlToImage ||
             "https://img.pngio.com/news-png-15-clip-arts-for-free-download-on-een-2019-news-png-1920_807.png"
           }
         />
-        <div>
+        <div className={classes.details}>
           <Typography variant="body2" color="textSecondary" component="h2">
             {new Date(publishedAt).toDateString()}
           </Typography>
@@ -33,7 +46,7 @@ const NewsCard = ({
             {source.name}
           </Typography>
         </div>
-        <Typography gutterBottom variant="h5">
+        <Typography className={classes.title} gutterBottom variant="h5">
           {title}
         </Typography>
         <CardContent>
@@ -42,7 +55,7 @@ const NewsCard = ({
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
+      <CardActions className={classes.cardActions}>
         <Button size="small" color="primary">
           Learn More
         </Button>
